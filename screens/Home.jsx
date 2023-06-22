a631c264b162f7e205a1fb22c53b5326a4481bee
Kostia1390 <71783611+Kostia1390@users.noreply.github.com>
2023-06-22 16:55:41 +0300

    AddNavigation
    AddNavigation

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export const Home = () => {
+export const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>app!!</Text>
+      <Text>Kostia Vashchenko app!!</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+        <Image source={require('../assets/svg/photo.png')} style={{ width: 200, height: 200 }} />
+      </TouchableOpacity>
     </View>
   );
 };
