344bf18a54eb119d6d6a85be9a053070fe483368
Kostia1390 <71783611+Kostia1390@users.noreply.github.com>
2023-06-23 17:27:30 +0300

    addTextInput

---
+++ src/components/BottomButton.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import styled, { css } from '@emotion/native';
+
+const BottomButton = () => {
+  return (
+    <MainContainer>
+      <Image source={require('../../assets/svg/Button1.png')} style={{ width: 24, height: 24 }} />
+      <Image source={require('../../assets/svg/Button2.png')} style={{ width: 24, height: 24 }} />
+      <Image source={require('../../assets/svg/Button3.png')} style={{ width: 26, height: 24 }} />
+      <Image source={require('../../assets/svg/Button4.png')} style={{ width: 24, height: 24 }} />
+    </MainContainer>
+  );
+};
+
+const MainContainer = styled(View)`
+  padding: 20px 50px 10px 50px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default BottomButton;
